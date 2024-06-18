a2f22b0479bc1b924f687ac5068696f184af818e
Kroc Camen <kroc@camendesign.com>
2024-06-18 21:21:36 +0100

    Add NTVCM for automation
    RunCPM's lack of automation has been a problem, for sure, but it does have a good debugger.
    NTVCM also runs CP/M binaries on the command line, but it's a proper command-line tool rather than a self-contained emulator that includes the CCP.
    NTVCM allows us to completely automate building v80 and then using v80 to build other stuff; I still can't get RunCPM to automate its input.

--- build.bat
+++ build.bat
@@ -5,13 +5,18 @@ ECHO:
 
 SET "BIN_DIR=bin"
 
-REM # the assembler and linker is WLA-DX
+REM # the PC assembler and linker is WLA-DX
 SET WLA_Z80="%BIN_DIR%\wla-dx\wla-z80.exe"  -x -I "src"
 SET WLA_LINK="%BIN_DIR%\wla-dx\wlalink.exe" -A -S
 
 REM # RunCPM "emulator"
-SET "CPM_DIR=%BIN_DIR%\RunCPM"
-SET RUN_CPM="%CPM_DIR%\RunCPM.exe"
+SET "DIR_RUNCPM=%BIN_DIR%\RunCPM"
+SET "BIN_RUNCPM=%DIR_RUNCPM%\RunCPM.exe"
+
+REM # NTVCM, CP/M Virtual Machine
+REM # https://github.com/davidly/ntvcm
+SET "DIR_NTVCM=%BIN_DIR%\ntvcm"
+SET "BIN_NTVCM=ntvcm.exe -l"
 
 IF NOT EXIST "build" MKDIR "build"
 
@@ -27,14 +32,30 @@ IF ERRORLEVEL 1 EXIT /B 1
 
 IF ERRORLEVEL 1 EXIT /B 1
 
-IF NOT EXIST "%CPM_DIR%\A\0" MKDIR "%CPM_DIR%\A\0"
-DEL /F /Q "%CPM_DIR%\A\0\*.*"
-
-REM # copy the COM files into the CP/M disk directory
+REM # copy the COM files into the NTVCM disk directory
 REM # "/N" forces an 8.3 file-name in the destination
-COPY /N /Y "build\*.com" /B "%CPM_DIR%\A\0" /B
+COPY /N /Y "build\*.com" /B "%DIR_NTVCM%" /B
 REM # copy test v80 files
-COPY /N /Y "test\*.v80" /A "%CPM_DIR%\A\0" /A
+COPY /N /Y "test\*.v80" /A "%DIR_NTVCM%" /A
+
+ECHO:
+
+REM # run NTVCM
+PUSHD "%DIR_NTVCM%"
+
+%BIN_NTVCM% v80.com test.v80
+
+POPD
+
+IF NOT EXIST "%DIR_RUNCPM%\A\0" MKDIR "%DIR_RUNCPM%\A\0"
+DEL /F /Q "%DIR_RUNCPM%\A\0\*.*"
+
+REM # copy the COM files into the RunCPM disk directory
+REM # "/N" forces an 8.3 file-name in the destination
+COPY /N /Y "build\*.com" /B "%DIR_RUNCPM%\A\0" /B
+REM # copy test v80 files
+COPY /N /Y "test\*.v80" /A "%DIR_RUNCPM%\A\0" /A
 
 REM # run the CP/M emulator
-START "RunCPM" /D "%CPM_DIR%" %RUN_CPM%
+START "RunCPM" /D "%DIR_RUNCPM%" %BIN_RUNCPM%
+
