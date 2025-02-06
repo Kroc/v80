deb98b4b748fd4c51bea7fa7d5981eb6d9f596a2
Kroc Camen <kroc@camendesign.com>
2025-02-06 12:01:59 +0000

    Just enough Linux to be dangerous
    Converted enough of the build script to get to the part where the new ISA parsing is halting. I didn't want to mix two breaking changes like this but I wasn't expecting to switch to Linux.

--- build.sh
+++ build.sh
@@ -7,20 +7,20 @@ set -e
 # binaries we'll be using
 #
 ntvcm=build/ntvcm/ntvcm
-wladxz80=build/wla-dx/binaries/wla-z80
-wladx6502=build/wla-dx/binaries/wla-6502
-wlalink=build/wla-dx/binaries/wlalink
+wla_z80=build/wla-dx/binaries/wla-z80
+wla_6502=build/wla-dx/binaries/wla-6502
+wla_link=build/wla-dx/binaries/wlalink
 runcpm=build/runcpm/RunCPM/RunCPM
 
 
 # download and build NTVCM, if not present
 #
-if ! [ -e build/ntvcm ]; then
+if ! [ -d build/ntvcm ]; then
         echo Cloning NTVCM...
         git clone --recurse-submodules \
 		https://github.com/davidly/ntvcm.git build/ntvcm
 fi
-if ! [ -e $ntvcm ]; then
+if ! [ -f $ntvcm ]; then
         echo Building NTVCM...
         cd build/ntvcm
         ./mr.sh
@@ -29,13 +29,13 @@ fi
 
 # download and build WLA-DX, if not present
 #
-if ! [ -e build/wla-dx ]; then
+if ! [ -d build/wla-dx ]; then
         echo Cloning WLA-DX...
         git clone --recurse-submodules \
                 https://github.com/vhelin/wla-dx.git build/wla-dx
 fi
 # WLA-DX uses cmake so if the makefile is not present, generate it:
-if ! [ -e build/wla-dx/Makefile ]; then
+if ! [ -f build/wla-dx/Makefile ]; then
         echo Generating WLA-DX Makefiles...
         cd build/wla-dx
         # (from INSTALL in build/wla-dx)
@@ -43,7 +43,7 @@ if ! [ -e build/wla-dx/Makefile ]; then
         cd ../..
 fi
 # lastly if the binary doesn't exist, compile it
-if ! [ -e $wlalink ]; then
+if ! [ -f $wla_link ]; then
         echo Building WLA-DX...
         pwd
         cd build/wla-dx
@@ -53,21 +53,77 @@ fi
 
 # download and build RunCPM, if not present
 #
-if ! [ -e build/runcpm ]; then
+if ! [ -d build/runcpm ]; then
         echo Cloning RunCPM...
         git clone --recurse-submodules \
                 https://github.com/MockbaTheBorg/RunCPM.git build/runcpm
 fi
-if ! [ -e $runcpm ]; then
+if ! [ -f $runcpm ]; then
         echo Building RunCPM...
         cd build/runcpm/RunCPM
+        # NOTE: Mac requires "macosx" instead of "posix" here
         make posix build
         cd ../../..
 fi
-
 # delete previous build files (but not the sub-directories)
 echo Clean build files...
 rm -f build/*.*
- 
+
+# ensure the virtual floppy-disk folder exists
+mkdir -p build/runcpm/RunCPM/A/0
+# clear RunCPM temp files
+rm -f build/runcpm/RunCPM/A/0/*.*
+
+# build v80 assembler [v0] from WLA-DX source:
+# ==============================================================================
+# this builds "v80_wla.com", the WLA version of v80
+
+# assemble,
+$wla_z80 -v -I "v0" -o "build/v80_wla.o" "v80.wla"
+# and link
+$wla_link -v -b "v0/link.ini" "build/v80_wla.com"
+
+echo
+
+# build and run tests...
+# ------------------------------------------------------------------------------
+# function: assemble .wla source with WLA-DX-Z80
+wla_z80() {
+        echo "* wla-z80 $1.wla"
+
+        # assmeble file to test.o, regardless of input name
+        $wla_z80 -I "test" -o "build/test.o" "test/$1.wla"
+        # link test.o to input-named *_wla.com file
+        $wla_link -b "test/link.ini" "build/$1_wla.com"
+}
+
+# function: assemble a .v80 file using the WLA-built [v0] version of v80
+v80_wla() {
+        echo "* v80_wla.com $1 $2"
+        cd build
+        ./ntvcm/ntvcm -l v80_wla.com "$1" "$2" # > "${1%.*}.sym"
+        cd ..
+}
+
+# function: run a comparison between WLA-DX-Z80 [PC] & v80_wla [v0]
+run_test_wla() {
+        # build "$1.wla" with WLA-DX [PC],
+        wla_z80 "$1"
+        # and "$1.v80" with v80_wla [v0]
+        v80_wla "$1"  
+}
+
+# copy test v80/v65 files to build directory for running tests
+cp test/*.v?? build
+
+# copy the COM files into the RunCPM disk directory
+cp build/*.com build/runcpm/RunCPM/A/0
+# copy test v80/v65 files
+cp test/*.v?? build/runcpm/RunCPM/A/0
+
+run_test_wla "z80"
+
 echo
 echo OK.
+exit 0
+
